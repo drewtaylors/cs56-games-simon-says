08b4858db9b7906b09dd33e32886b528796442e2
Shanen Cross <shanencross@umail.ucsb.edu>
2013-05-30 01:18:17 -0700

    Added build.xml file that works and runs a class which generates an empty java frame

---
+++ src/edu/ucsb/cs56/projects/games/simon_says/SimonFrame.java
@@ -0,0 +1,23 @@
+//package edu.ucsb.cs56.projects.games.simon_says;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class SimonFrame extends JFrame {
+
+    /** A no-arg constructor which builds the skeleton for the
+     *  game's frame, generating an empty frame of a particular size. 
+     */
+
+    public SimonFrame() {
+	super("Simon Says"); 
+	this.setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE);
+	this.setSize(640,480);
+	this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+	SimonFrame frame = new SimonFrame();
+    }
+}

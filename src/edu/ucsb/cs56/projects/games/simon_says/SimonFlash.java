51376930957eefb3f630dfb862beab483312da6a
Shanen Cross <shanencross@umail.ucsb.edu>
2013-06-07 16:12:26 -0700

    overhauled SimonFlash class structure

--- src/edu/ucsb/cs56/projects/games/simon_says/SimonFlash.java
+++ src/edu/ucsb/cs56/projects/games/simon_says/SimonFlash.java
@@ -11,18 +11,40 @@ import java.util.*;
 
 public class SimonFlash
  {
+     private ArrayList<Integer>  userButtonPresses;
+     private ArrayList<Integer> computerButtonPresses;
+     private SimonButton[] buttons;
+
+     public static void  FlashSequence(ArrayList<Integer> flashes, SimonButton[] buttons) {
+	 SimonFlash sequence = new SimonFlash(flashes, buttons);
+	 sequence.go();
+     }
+
+     public SimonFlash() { 
+	 userButtonPresses = new ArrayList<Integer>();
+	 computerButtonPresses = new ArrayList<Integer>();
+	 buttons = new SimonButton[4];
+     }
+
+     public SimonFlash(ArrayList<Integer> flashes, SimonButton[] buttons) {
+	 userButtonPresses = new ArrayList<Integer>(); // points all instance variables to same arrays passed in
+	 this.computerButtonPresses = flashes;         // no duplication
+	 this.buttons = buttons;
+     }
+
+
      /** Flashes in sequential order a sequence of numbers
      */
-     public static void  FlashSequence(ArrayList<Integer> flashes, SimonButton[] buttons) {
+     public void go() {
 	 try {
 	     for (SimonButton button : buttons) {
 		 button.setEnabled(false);
+		 button.removeActionListeners();
 	     }
-	     for (int button_num : flashes) { // iterate through each sequence element
+	     for (int button_num : computerButtonPresses) { // iterate through each sequence element
 		 Thread.sleep(300);
 		 SimonButton button = buttons[button_num]; // for readiblity
-		 // buttons[flash].doClick(5000);
-		 // DEBUG
+		 
 		 Color buttonColor = button.getBackground();	 
 	         button.setBackground(Color.WHITE);
 		 Thread.sleep(150);
@@ -31,11 +53,17 @@ public class SimonFlash
 	 } catch (InterruptedException ex) { ex.printStackTrace(); }
 
 	 for (SimonButton button : buttons) {
+	     button.addActionListener(new ButtonPushListener());
 	     button.setEnabled(true);
-
 	 }
-
-	 
+	 userButtonPresses = new ArrayList<Integer>();
      }
+
+     public class ButtonPushListener implements ActionListener {
+	 public void actionPerformed(ActionEvent e) {
+	     
+	 }
+     }
+
 } 
 
